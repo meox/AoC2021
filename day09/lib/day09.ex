f039c36c45ee813c606e94ccf9d387e201b82e46
Gian Lorenzo Meocci <glmeocci@gmail.com>
2021-12-08 21:08:27 +0100

    scaffolding

--- day09/lib/day09.ex
+++ day09/lib/day09.ex
@@ -2,4 +2,13 @@ defmodule Day09 do
   @moduledoc """
   Documentation for `Day09`.
   """
+
+  def load_input() do
+    {:ok, content} = File.load("./dat/input.txt")
+
+    content
+    |> String.split("\n", trim: true)
+
+  end
+
 end
