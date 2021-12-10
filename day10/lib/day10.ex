aa0c397dd1c788a413f32c64f2033224ca269d9c
Gian Lorenzo Meocci <glmeocci@gmail.com>
2021-12-10 17:09:50 +0100

    Day10: part1 + part2

--- day10/lib/day10.ex
+++ day10/lib/day10.ex
@@ -3,16 +3,111 @@ defmodule Day10 do
   Documentation for `Day10`.
   """
 
-  @doc """
-  Hello world.
+  def part1(input) do
+    input
+    |> Enum.reduce(
+      0,
+      fn line, acc ->
+        score =
+          line
+          |> parse()
+          |> score_syntax_error()
+        score + acc
+      end
+    )
+  end
 
-  ## Examples
+  def part2(input) do
+    scores =
+      input
+      |> Enum.map(&parse/1)
+      |> Enum.filter(
+        fn
+          [] ->
+            false
+          {:expected, _, _} ->
+            false
+          _ ->
+            true
+        end
+      )
+      |> Enum.map(
+        fn incomplete ->
+          incomplete
+          |> Enum.map(&closing/1)
+          |> auto_complete_score()
+        end
+      )
+      |> Enum.sort()
 
-      iex> Day10.hello()
-      :world
+    Enum.at(scores, trunc(length(scores)/2))
+  end
 
-  """
-  def hello do
-    :world
+  def auto_complete_score(ps) do
+    ps
+    |> Enum.reduce(
+      0,
+      fn
+        ")", acc ->
+          acc*5 + 1
+        "]", acc ->
+          acc*5 + 2
+        "}", acc ->
+          acc*5 + 3
+        ">", acc ->
+          acc*5 + 4
+      end
+    )
+  end
+
+  def closing("("), do: ")"
+  def closing("["), do: "]"
+  def closing("{"), do: "}"
+  def closing("<"), do: ">"
+
+  def score_syntax_error({:expected, _, ")"}), do: 3
+  def score_syntax_error({:expected, _, "]"}), do: 57
+  def score_syntax_error({:expected, _, "}"}), do: 1197
+  def score_syntax_error({:expected, _, ">"}), do: 25137
+  def score_syntax_error(_), do: 0
+
+  def parse(line) do
+    line
+    |> String.split("", trim: true)
+    |> Enum.reduce_while(
+      [],
+      fn
+        ch, stack when ch in ["(", "[", "{", "<"]->
+          {:cont, [ch | stack]}
+        _, [] ->
+          {:halt, {:error, :not_completed}}
+        ")", ["(" | rest] ->
+          {:cont, rest}
+        "]", ["[" | rest] ->
+          {:cont, rest}
+        "}", ["{" | rest] ->
+            {:cont, rest}
+        ">", ["<" | rest] ->
+          {:cont, rest}
+        ch, ["(" | _] ->
+          {:halt, {:expected, ")", ch}}
+        ch, ["[" | _] ->
+          {:halt, {:expected, "]", ch}}
+        ch, ["{" | _] ->
+          {:halt, {:expected, "}", ch}}
+        ch, ["<" | _] ->
+          {:halt, {:expected, ">", ch}}
+      end
+    )
+  end
+
+  def load_sample(), do: load_input("./data/sample.txt")
+  def load_input(), do: load_input("./data/input.txt")
+
+  def load_input(filename) do
+    {:ok, content} = File.read(filename)
+
+    content
+    |> String.split("\n", trim: true)
   end
 end
--- day10/test/day10_test.exs
+++ day10/test/day10_test.exs
@@ -2,7 +2,4 @@ defmodule Day10Test do
   use ExUnit.Case
   doctest Day10
 
-  test "greets the world" do
-    assert Day10.hello() == :world
-  end
 end
